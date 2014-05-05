c316ab1aefd408d696f8bc1c5fd217c6dbf8a7a9
Robert Brendler <robert.brendler@gmail.com>
2014-05-05 13:37:51 +0200

    made postfix install non interactive

--- scripts/install_postfix.sh
+++ scripts/install_postfix.sh
@@ -4,7 +4,12 @@
 if [[ -z "$POSTFIX_MAILNAME" ]] && [[ -z "$POSTFIX_MAILERTYPE" ]] ; then
 	sudo debconf-set-selections <<< "postfix postfix/mailname string $POSTFIX_MAILNAME"
 	sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string $POSTFIX_MAILERTYPE"
+	
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
+	
+	else
+	
+	sudo apt-get install -y postfix
 fi
 
-sudo apt-get install -y postfix
 

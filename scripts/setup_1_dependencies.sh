fea63d3fb5c40457730f233b44deb15047cee072
Robert Brendler <robert.brendler@gmail.com>
2014-05-05 13:16:24 +0200

    added apt-get update/upgrade

--- scripts/setup_1_dependencies.sh
+++ scripts/setup_1_dependencies.sh
@@ -7,6 +7,9 @@ echo "###### 1. Setup dependencies                      ######"
 echo "########################################################"
 echo
 
+sudo apt-get update
+sudo apt-get upgrade
+
 sudo apt-get install -y build-essential zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libncurses5-dev libffi-dev curl openssh-server redis-server checkinstall libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev logrotate
 
 # Install Git

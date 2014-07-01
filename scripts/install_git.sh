deb6a54e2a3bd7237ca24e6fa0c3abdb888af13f
Robert Brendler <robert.brendler@iteratec.de>
2014-07-01 17:23:33 +0200

    unpacked archive has different naming

--- scripts/install_git.sh
+++ scripts/install_git.sh
@@ -53,8 +53,8 @@ else
 	cd /tmp
 	rm -rf git-v$GIT_TARGET_VERSION*
 	wget https://github.com/git/git/archive/v$GIT_TARGET_VERSION.tar.gz
-	tar xzf git-v$GIT_TARGET_VERSION.tar.gz
-	cd git-v$GIT_TARGET_VERSION/
+	tar xzf v$GIT_TARGET_VERSION.tar.gz
+	cd git-$GIT_TARGET_VERSION/
 	make --silent prefix=/usr/local all
 
 	# Install into /usr/local/bin
